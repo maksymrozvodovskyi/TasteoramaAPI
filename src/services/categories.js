12038e01ebe8f5cc35d4c5c6e121ba8e9e0e79c1
Maksym Boiko <Maksimboiko222@gmail.com>
2025-08-20 18:27:26 +0200

    fix

--- src/services/categories.js
+++ src/services/categories.js
@@ -1,5 +1,5 @@
 import { CategoriesCollection } from '../db/models/category.js';
 
-export const getAllCategories = () => {
-  return CategoriesCollection.find();
+export const getAllCategories = async () => {
+  return await CategoriesCollection.find();
 };
