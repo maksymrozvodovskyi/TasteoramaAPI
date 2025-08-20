4db00fe8f2118ed0cc3cc59081dff5c76fc5679a
Lina Parashchenko <blond2109@gmail.com>
2025-08-20 23:41:40 +0300

    add favorites recipes

---
+++ src/controllers/favouritesController.js
@@ -0,0 +1,27 @@
+import { RecipesCollection } from '../db/models/recipe.js';
+
+export const addToFavorites = async (req, res) => {
+  const { recipeId } = req.params;
+  const user = req.user;
+
+  const recipe = await RecipesCollection.findById(recipeId);
+  if (!recipe) {
+    return res.status(404).json({ message: 'Recipe not found' });
+  }
+
+  const alreadyInFavorites = user.favouriteRecipes.some(
+    (id) => id.toString() === recipeId,
+  );
+
+  if (alreadyInFavorites) {
+    return res.status(400).json({ message: 'Already in favorites' });
+  }
+
+  user.favouriteRecipes.push(recipe._id);
+  await user.save();
+
+  res.status(200).json({
+    message: 'Recipe added to favorites',
+    favorites: user.favouriteRecipes,
+  });
+};
--- src/routes/recipesRouter.js
+++ src/routes/recipesRouter.js
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import { addToFavorites } from '../controllers/favouritesController.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
+
+router.post('/favorites/:recipeId', authenticate, addToFavorites);
 
 export default router;
