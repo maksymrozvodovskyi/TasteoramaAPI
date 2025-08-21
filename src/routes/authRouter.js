970198dcc3aef07c70adb885bd3e571e736e35fa
Bohdan <toyvo7@gmail.com>
2025-08-21 17:36:56 +0200

    add authLogin

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,5 +1,15 @@
 import { Router } from 'express';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { loginSchema } from '../validation/auth.validation.js';
+import { loginUserController } from '../controllers/authLogin.js';
 
 const router = Router();
+
+router.post(
+  '/login',
+  validateBody(loginSchema),
+  ctrlWrapper(loginUserController),
+);
 
 export default router;
