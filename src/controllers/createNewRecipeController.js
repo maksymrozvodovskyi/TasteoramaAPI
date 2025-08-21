7077933b72b97ae7001ae5bbe7e5718f658e6506
Volodymyr_Rostovtsev <fortik1510@gmail.com>
2025-08-21 13:23:36 +0200

    createNewRecipe

---
+++ src/controllers/createNewRecipeController.js
@@ -0,0 +1,19 @@
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { createNewRecipe } from '../services/createNewRecipe.js';
+
+
+export const createNewRecipeController = async (req, res, next) => {
+    const photo = req.file;
+    let photoUrl;
+    if (photo) {
+        photoUrl = await saveFileToCloudinary(photo);
+    }
+
+    const recipe = await createNewRecipe({...req.body, /*userId: req.user._id, */photo: photoUrl,});
+
+    res.status(201).json({
+        status: 201,
+        message: "Successfully created a recipe!",
+        data: recipe,
+    });
+};
--- src/routes/recipesRouter.js
+++ src/routes/recipesRouter.js
@@ -1,5 +1,17 @@
 import { Router } from 'express';
+import { authenticate } from '../middlewares/authenticate.js';
+import { ctrlWrapper } from "../utils/ctrlWrapper.js";
+import { validateBody } from "../middlewares/validateBody.js";
+import { upload } from '../middlewares/multer.js';
+import { createRecipeSchema } from '../validation/recipe.validation.js';
+import { createNewRecipeController } from '../controllers/createNewRecipeController.js';
 
 const router = Router();
+
+router.post('/', upload.single('recipeImg'), validateBody(createRecipeSchema), /*authenticate,*/ ctrlWrapper(createNewRecipeController));
+
+
+
+
 
 export default router;
