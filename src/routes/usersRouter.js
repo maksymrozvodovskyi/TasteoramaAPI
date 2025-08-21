fbfeb9225a126ec6b8bb0bcec4c6bc08153724ac
Maksym Boiko <Maksimboiko222@gmail.com>
2025-08-22 01:38:51 +0200

    create private endpoint

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,5 +1,11 @@
 import { Router } from 'express';
+import { authenticate } from '../middlewares/authenticate.js';
+import { getUserController } from '../controllers/users.js';
 
 const router = Router();
+
+router.use(authenticate);
+
+router.get('/', getUserController);
 
 export default router;
