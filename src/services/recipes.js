b8a9faf098ccf530e807b160077852d1afc66e7a
tetiana_hushnovska <thushnyovska@gmail.com>
2025-08-21 22:51:31 +0100

    fix server

--- src/services/recipes.js
+++ src/services/recipes.js
@@ -1,5 +1,5 @@
 import { RecipesCollection } from '../db/models/recipe.js';
 
 export const getRecipeById = async (recipeId) => {
-  return RecipesCollection.findById(recipeId);
+  return RecipesCollection.findOne({ _id: recipeId });
 };
