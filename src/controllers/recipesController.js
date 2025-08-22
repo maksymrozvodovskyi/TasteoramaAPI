22393f3e25199c88a8bbfa85c2d68dda2a5ec2a8
Sergeyt2000 <sergeyt2000@gmail.com>
2025-08-22 15:13:05 +0300

    fix

--- src/controllers/recipesController.js
+++ src/controllers/recipesController.js
@@ -2,16 +2,22 @@ import createHttpError from 'http-errors';
 import { deleteFavoriteRecipe } from '../services/recipesServices.js';
 
 export async function deleteFavoriteRecipeController(req, res, next) {
-  const userId = req.user._id;
+  try {
+    const userId = req.user._id;
+    const { recipeId } = req.params;
 
-  const { recipeId } = req.params;
-  const user = await deleteFavoriteRecipe(userId, recipeId);
-  if (!user) {
-    throw createHttpError(404, 'Recipe not found or access denied');
+    const updatedUser = await deleteFavoriteRecipe(userId, recipeId);
+    if (!updatedUser) {
+      return next(
+        createHttpError(404, 'Recipe not found in favorites or access denied'),
+      );
+    }
+    res.status(200).json({
+      status: 200,
+      message: 'Recipe removed from favorites',
+      // favouriteRecipes: updatedUser.favouriteRecipes,
+    });
+  } catch (error) {
+    next(error);
   }
-  res.status(200).json({
-    status: 200,
-    message: 'Recipe removed from favorites',
-    // data: user,
-  });
 }
--- src/services/recipesServices.js
+++ src/services/recipesServices.js
@@ -2,15 +2,20 @@ import { UsersCollection } from '../db/models/user.js';
 
 export async function deleteFavoriteRecipe(userId, recipeId) {
   const user = await UsersCollection.findById(userId);
-  const updatedFavorites = user.favouriteRecipes.filter(
-    (id) => id.toString() !== recipeId,
-  );
+  if (!user) return null;
+
+  const current = user.favouriteRecipes || [];
+  const updatedFavorites = current.filter((id) => id.toString() !== recipeId);
+
+  const isChanged = updatedFavorites.length !== current.length;
+  if (!isChanged) return null;
 
   const updatedUser = await UsersCollection.findByIdAndUpdate(
     userId,
     { favouriteRecipes: updatedFavorites },
     { new: true },
   );
+
 
   return updatedUser;
 }
