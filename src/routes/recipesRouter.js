46d8633f7f54734e38ddce3cfdc1599512d65b9b
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-08-22 19:13:38 +0300

    save

--- src/routes/recipesRouter.js
+++ src/routes/recipesRouter.js
@@ -1,13 +1,12 @@
 import { Router } from 'express';
 import { authenticate } from '../middlewares/authenticate.js';
-import { ctrlWrapper } from "../utils/ctrlWrapper.js";
-import { validateBody } from "../middlewares/validateBody.js";
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
 import { upload } from '../middlewares/multer.js';
 import { createRecipeSchema } from '../validation/recipe.validation.js';
 import { createNewRecipeController } from '../controllers/createNewRecipeController.js';
 import { parseFormDataJson } from '../middlewares/parseFormDataJson.js';
 import { deleteFavoriteRecipeController } from '../controllers/recipesController.js';
-import { validateId } from '../middlewares/validateID.js';
 import { addToFavorites } from '../controllers/favouritesController.js';
 import { getRecipeByIdController } from '../controllers/recipes.js';
 import { isValidId } from '../middlewares/isValidId.js';
@@ -19,10 +18,24 @@ router.get('/:id', isValidId, ctrlWrapper(getRecipeByIdController));
 router.delete(
   '/favorites/:recipeId',
   authenticate,
-  validateId,
+  isValidId,
   ctrlWrapper(deleteFavoriteRecipeController),
 );
 
-router.post('/', upload.single('thumb'), parseFormDataJson, validateBody(createRecipeSchema), authenticate, ctrlWrapper(createNewRecipeController));
+router.post(
+  '/favorites/:recipeId',
+  isValidId,
+  authenticate,
+  ctrlWrapper(addToFavorites),
+);
+
+router.post(
+  '/',
+  upload.single('thumb'),
+  parseFormDataJson,
+  validateBody(createRecipeSchema),
+  authenticate,
+  ctrlWrapper(createNewRecipeController),
+);
 
 export default router;
