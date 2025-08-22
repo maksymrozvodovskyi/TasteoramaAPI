449549beb3bc153e73c7e8d1618bbf1f6e9fcd84
Maksym Boiko <Maksimboiko222@gmail.com>
2025-08-22 18:18:51 +0200

    fix

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,7 +1,7 @@
 export const getUserController = async (req, res) => {
   const user = req.user;
 
-  res.json({
+  res.status(200).json({
     status: 200,
     message: 'Successfully found user!',
     data: user,
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { authenticate } from '../middlewares/authenticate.js';
 import { getUserController } from '../controllers/users.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
 router.use(authenticate);
 
-router.get('/', getUserController);
+router.get('/', ctrlWrapper(getUserController));
 
 export default router;
